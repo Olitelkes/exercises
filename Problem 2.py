891cadc8ddb9ea06140dfd88f662a33086d07c7c
Olitelkes <rigole.olivier@gmail.com>
2021-10-24 14:12:49 -0400

    add problem 2

---
+++ Problem 2.py
@@ -0,0 +1,20 @@
+# Given an array of integers, return a new array such that each element at index i of the
+# new array is the product of all the numbers in the original array except the one at i.
+
+# For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+# If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+def add_list(mylist):
+    for x in mylist:
+        res = 1
+        i = 0
+        for y in mylist:
+            if mylist.index(x) != i:
+                res = y * res
+            i = i + 1
+
+        print(res)
+
+
+if __name__ == '__main__':
+    add_list([1, 2, 3, 4, 5])
